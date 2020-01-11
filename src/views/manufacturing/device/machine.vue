c11e7e9ea94042229096f262c199dc6018fa8e98
moling <365024424@qq.com>
2020-01-11 17:06:24 +0800

    编写页面骨架

---
+++ src/views/manufacturing/device/machine.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="Machine">
+    Machine
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Machine',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
