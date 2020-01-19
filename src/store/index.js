ea55143cddac176cb201893de5d299481a7e563f
moling <365024424@qq.com>
2020-01-19 14:02:41 +0800

    store热加载

--- src/store/index.js
+++ src/store/index.js
@@ -9,4 +9,13 @@ const store = new Vuex.Store({
   modules: getStoreModules(moduleContext),
 })
 
+if (module.hot) {
+  const moduleKeys = moduleContext.keys().map(path => `./modules${path.split('.')[1]}`)
+  module.hot.accept(moduleKeys, () => {
+    store.hotUpdate({
+      modules: getStoreModules(moduleContext),
+    })
+  })
+}
+
 export default store
