7c4b5039d7819cfacd1487773616a7d76db6cd9b
moling <365024424@qq.com>
2020-01-19 14:16:04 +0800

    边栏的模块选择开发完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,12 @@
       <div class="logo">Smart</div>
     </div>
     <div class="body">
-      <div class="sidebar"/>
+      <div class="sidebar">
+        <div v-for="m in modules" :key="m.code" class="module" :class="{ active: m.code === activeModule }" @click="moduleChange(m)">
+          <i class="iconfont" :class="`icon-${m.icon}`"/>
+          <p class="text">{{ m.name }}</p>
+        </div>
+      </div>
       <div class="menu"/>
       <div class="main">
         <div class="tags"/>
@@ -15,8 +20,31 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'Home',
+  data () {
+    return {
+      activeModule: '',
+    }
+  },
+  computed: {
+    ...mapGetters(['modules']),
+  },
+  watch: {
+    // 根据路由动态切换模块样式
+    '$route.fullPath': {
+      handler (fullPath) {
+        this.activeModule = fullPath.split('/')[1]
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    moduleChange ({ route }) {
+      this.$router.push({ name: route })
+    },
+  },
 }
 </script>
 
@@ -42,7 +70,25 @@ export default {
     display: flex;
     .sidebar {
       flex-basis: 70px;
-      background: #2c3d59;
+      background-color: #2c3d59;
+      .module {
+        border-left: 3px solid #2c3d59;
+        margin: 30px 0;
+        text-align: center;
+        color: rgba(255, 255, 255, 0.6);
+        cursor: pointer;
+        .iconfont {
+          font-size: 30px;
+        }
+        .text {
+          margin-top: 5px;
+          font-size: 10px;
+        }
+        &.active {
+          color: #fff;
+          border-color: #fff;
+        }
+      }
     }
     .menu {
       flex-basis: 230px;
