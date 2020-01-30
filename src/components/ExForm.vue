5b263d42fa2e2a07dfa08b4453a27e7def81d8cf
moling <365024424@qq.com>
2020-01-30 23:49:26 +0800

    添加ExForm组件

---
+++ src/components/ExForm.vue
@@ -0,0 +1,125 @@
+<template>
+  <el-form ref="form" class="ex-form" :model="form" :rules="rules" v-bind="$attrs" label-width="auto">
+    <el-row :gutter="20">
+      <el-col v-for="item in formItemList" :key="item.value" :span="item.span">
+        <el-form-item :label="item.label" :prop="item.value">
+          <component :is="item.component" v-model.trim="form[item.value]" v-bind="item.attrs" @change="fieldChange(item.value)"/>
+        </el-form-item>
+      </el-col>
+      <el-col :span="24">
+        <el-form-item>
+          <el-button type="primary" @click="submit">提交</el-button>
+        </el-form-item>
+      </el-col>
+    </el-row>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: 'ExForm',
+  props: {
+    // 表单默认值
+    formData: {
+      type: Object,
+      required: true,
+    },
+    // 表单元素
+    formItems: {
+      type: Object,
+      required: true,
+    },
+    // 表单规则
+    rules: {
+      type: Object,
+      default () {
+        return {}
+      },
+    },
+    // 表单字段改变的回调事件
+    changeEvents: {
+      type: Object,
+      default () {
+        return {}
+      },
+    },
+  },
+  data () {
+    return {
+      form: {},
+    }
+  },
+  computed: {
+    // 将表单元素对象转成列表
+    formItemList () {
+      const keys = Object.keys(this.formItems)
+      return keys.map(key => {
+        const { label, component, span = 24, ...attrs } = this.formItems[key]
+        return {
+          value: key,
+          component,
+          label,
+          span,
+          attrs,
+        }
+      })
+    },
+  },
+  watch: {
+    // 监听formData改变重置表单字段值
+    formData: {
+      handler () {
+        this._resetForm()
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    // 克隆对象的辅助方法
+    _clone (obj) {
+      return JSON.parse(JSON.stringify(obj))
+    },
+
+    // 表单字段重置成默认值
+    _resetForm () {
+      const form = {}
+      for (const key in this.formItems) {
+        if (this.formItems[key].component === 'ex-select') {
+          form[key] = this.formItems[key].multiple ? [] : ''
+        }
+        if (key in this.formData) {
+          form[key] = this.formData[key]
+        }
+      }
+      this.form = form
+    },
+
+    // 监听表单字段的值变化
+    fieldChange (field) {
+      if (field in this.changeEvents) {
+        this.$nextTick(() => {
+          this.changeEvents[field](this.form, this.formItems, this.rules)
+        })
+      }
+    },
+
+    // 校验并提交表单
+    submit () {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.$emit('submit', this._clone(this.form))
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.ex-form {
+  margin: 0 10px;
+}
+</style>
